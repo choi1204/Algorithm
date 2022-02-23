6b4fbf1292a29bac2b262160953e9fc2301e3c2e
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-02-23 16:44:24 +0900

    49993번 해결

---
+++ study/src/main/java/algorithm/programmers/level2/Solution_49993.java
@@ -0,0 +1,38 @@
+package algorithm.programmers.level2;
+
+public class Solution_49993 {
+
+    public static int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for(String tech : skill_trees) {
+            int length = skill.length();
+            boolean[] skillArr = new boolean[length + 1];
+            boolean check = true;
+            skillArr[0] = true;
+
+            for (int i = 0; i < tech.length(); i++) {
+                char c = tech.charAt(i);
+                if (skill.contains(String.valueOf(c))) {
+                    int index = skill.indexOf(c);
+                    if (skillArr[index]) {
+                        skillArr[index + 1] = true;
+                    }
+                    else {
+                        check = false;
+                        break;
+                    }
+                }
+            }
+
+            answer = check == true ? answer + 1 : answer;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String skill = "CBD";
+        String[] skillTrees = { "BACDE", "CBADF", "AECB", "BDA" };
+        System.out.println(solution(skill,skillTrees));
+    }
+}
